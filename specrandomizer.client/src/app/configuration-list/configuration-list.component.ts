31d5d254262fe92fe353a9a8e2dde4b478c9ae58
Michael Pina <mpina@catalyte.io>
2025-03-13 15:54:57 -0500

    Configurations Working

--- specrandomizer.client/src/app/configuration-list/configuration-list.component.ts
+++ specrandomizer.client/src/app/configuration-list/configuration-list.component.ts
@@ -1,7 +1,14 @@
+
+
+
+
+
 import { Component } from '@angular/core';
 import { IConfiguration } from '../player-list/Configuration';
 import { HttpClient } from '@angular/common/http';
 import { IPlayer } from '../player/player.model';
+import { CLASSLIST } from '../class-list/mock-list';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuration-list',
@@ -12,9 +19,10 @@ export class ConfigurationListComponent {
 
   configs: IConfiguration[] = [];
   playerList: IPlayer[] = [];
+  classes = CLASSLIST;
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
     const userid = localStorage.getItem('userId');
@@ -22,30 +30,28 @@ export class ConfigurationListComponent {
     this.http.get<any>('https://localhost:7174/api/Configuration/user/' + userid)
       .subscribe(data => {
         this.configs = data?.$values ? data.$values.map((config: { players: { $values: any; }; }) => ({
-        ...config,
-          players: config.players.map(player => ({
-            playerId: player.id || null,
-            playerName: player.name || '',
-            numbers: Array.isArray(player.numbers) ? player.numbers : []
+          ...config,
+          players: config.players?.$values.map((player: any): IPlayer => ({
+            id: player.playerId || null,
+            PlayerName: player.playerName || '',
+            SpecList: Array.isArray(player.specList?.$values) ? player.specList?.$values : []
           })),
           userId: userid
-         
+
         })) : [];
-        this.configs.forEach(config => {
-          config.players.forEach((playerData) => {
-            this.playerList.push({
-              id: playerData.playerId || null,
-              PlayerName: playerData.playerName || "",
-              SpecList: Array.isArray(playerData.specList?.$values) ? playerData.specList?.$values : []
-            });
-            playerData = this.playerList;
-            this.playerList = [];
-          });
-        });
-        
-        console.log(this.configs);
-        console.log(this.playerList);
+
       })
+  }
+
+  navigateWithGroup(config: IConfiguration) {
+    config.players.forEach((player: { SpecList: any[]; }) => {
+      player.SpecList.forEach((spec: number) => {
+        if (spec === 13) {
+          player.SpecList[player.SpecList.indexOf(spec)] = 12;
+        }
+      })
+    });
+    this.router.navigate(['/roulette'], { state: { configuration: config } });
   }
 
 }
